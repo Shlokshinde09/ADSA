aafd085354cfd1c27718a6077395af840fefdf02
Shlokshinde09 <shlok.2403744102@vcet.edu.in>
2025-07-29 23:18:58 +0530

    Add files via upload

---
+++ hashfunc.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class hashfunc{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+
+        String str = sc.nextLine();
+        int n = str.length();
+        int s = 0;
+        for(int i=0; i<n; i++){
+            s=s+ascii(str.charAt(i));
+
+        }
+        hashfunction(s,str);
+    }
+
+public static char ascii(char a){
+    int asciiVal=a;
+    return a;
+}
+public static void hashfunction(int s,String str){
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the size of Hash:");
+
+    int n = sc.nextInt();
+    String[] hash = new String[n];
+    int j=s%10;
+    hash[j]=str;
+    for(int i = 0; i<10; i++){
+        System.out.println(i+":"+hash[i]);
+    }
+}
+}
